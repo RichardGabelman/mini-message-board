3a92ec639ee118d120b77c1bd8018c4a704947e6
RichardGabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-02-03 17:44:42 -0800

    Setup basic index router

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Mini-Message-Board Express app - listening on port ${PORT}!`);
+});
