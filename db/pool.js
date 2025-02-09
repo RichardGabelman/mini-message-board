49e2ccd75747902e416a71af05742dac3d698a5b
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-02-09 03:18:37 -0800

    Test env vars

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,11 @@
 const { Pool } = require("pg");
 
+console.log("PGHOST:", process.env.PGHOST);
+console.log("PGUSER:", process.env.PGUSER);
+console.log("PGDATABASE:", process.env.PGDATABASE);
+console.log("PGPASSWORD:", process.env.PGPASSWORD ? "****" : "NOT SET");
+console.log("PGPORT:", process.env.PGPORT);
+
 // All of the following properties should be read from environment variables
 module.exports = new Pool({
   host: process.env.PGHOST, // or wherever the db is hosted
