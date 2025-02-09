3ec2570d835aaeab6532567a3e0ec9f030c731e5
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-02-09 03:28:43 -0800

    Remove debug database url print statement

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,5 @@
 const { Pool } = require("pg");
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
-
 // All of the following properties should be read from environment variables
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL
