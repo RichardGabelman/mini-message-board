183461882b384d8b03875ec7df74d6b34fc76799
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-02-09 03:24:24 -0800

    Change pool to use database url instead

--- db/pool.js
+++ db/pool.js
@@ -1,16 +1,9 @@
 const { Pool } = require("pg");
 
-console.log("PGHOST:", process.env.PGHOST);
-console.log("PGUSER:", process.env.PGUSER);
-console.log("PGDATABASE:", process.env.PGDATABASE);
-console.log("PGPASSWORD:", process.env.PGPASSWORD ? "****" : "NOT SET");
-console.log("PGPORT:", process.env.PGPORT);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 
 // All of the following properties should be read from environment variables
 module.exports = new Pool({
-  host: process.env.PGHOST, // or wherever the db is hosted
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT // The default port
+  connectionString: process.env.DATABASE_URL
 });
